c4d96ffcc92dfcf113e73d78d285f439801a2fec
shevyf <shevaunfrazier@googlemail.com>
2022-10-27 17:30:14 +0100

    set allowed hosts for local and aws

--- django_react_proj/settings.py
+++ django_react_proj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "34.253.209.144"]
 
 
 # Application definition
