35a149ea2ff41dd9e75447ce4b0cc14faaccda0a
shevyf <shevaunfrazier@googlemail.com>
2022-11-01 09:53:17 +0000

    updates to get this running on apache

--- programme-fe/src/constants/index.js
+++ programme-fe/src/constants/index.js
@@ -1,3 +1,3 @@
-export const ITEMS_API_URL = "http://localhost:8000/api/items/";
-export const ATTENDEES_API_URL = "http://localhost:8000/api/attendees/";
-export const LOCATIONS_API_URL = "http://localhost:8000/api/locations/";
+export const ITEMS_API_URL = "http://34.253.209.144:8000/api/items/";
+export const ATTENDEES_API_URL = "http://34.253.209.144:8000/api/attendees/";
+export const LOCATIONS_API_URL = "http://34.253.209.144:8000/api/locations/";
