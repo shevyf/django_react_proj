84a610ba24587565c1d34d843e4608e29b40eb24
shevyf <shevaunfrazier@googlemail.com>
2023-02-18 21:08:54 +0000

    update for changes on server

--- django_react_proj/settings.py
+++ django_react_proj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "34.253.209.144"]
+ALLOWED_HOSTS = ["localhost", "34.253.29.245", "3.249.246.37"]
 
 
 # Application definition
